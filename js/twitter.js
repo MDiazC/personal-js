dc64a26fe8ad45cbfa6885543ec4e6836dd4f69d
MDiazC <moisesdiazcantos@gmail.com>
2015-05-21 21:26:53 +0200

    commit left design

--- js/twitter.js
+++ js/twitter.js
@@ -1,6 +1,8 @@
 var userJSONPath = "https://raw.githubusercontent.com/MDiazC/socialnetwork-js-project/master/js/users.json";
-var postsJSONPath = "https://raw.githubusercontent.com/MDiazC/socialnetwork-js-project/blob/master/js/posts.json";
+var postsJSONPath = "https://raw.githubusercontent.com/MDiazC/socialnetwork-js-project/master/js/posts.json";
 var loggedUserId = 5;
+var usersData;
+var postsData;
 
 
 function showUpdate(){
@@ -10,36 +12,88 @@ function showUpdate(){
 function loadUserInfo(){
 var source   = $("#user-template").html();
 var template = Handlebars.compile(source);
-var context = {title: "My New Post", body: "This is my first post!"};
+var context = {user_image: '../'+usersData[loggedUserId].pic, user_name: usersData[loggedUserId].username, user_text: usersData[loggedUserId].about};
 var html    = template(context);
 $('#logged_user').append(html);
 }
 function loadPosts(){
-var source   = $("#entry-template").html();
+
+var source   = $("#posts-template").html();
 var template = Handlebars.compile(source);
-var context = {title: "My New Post", body: "This is my first post!"};
+
+var context=[];
+var subpost_info={};
+var subpost=[];
+var post={};
+var userId=0;
+
+for(var update in postsData){
+    userId=postsData[update].userId
+    post.user_image='../'+usersData[userId].pic;
+    post.user_text=usersData[userId].about;
+    post.user_name=usersData[userId].username;
+    post.user_post=postsData[update].content;
+
+    if(postsData[update].comments.length > 0){
+        subpost=[];
+        for(var comment in postsData[update].comments){
+            userId=postsData[update].comments[comment].userId;
+            subpost_info.user_image='../'+usersData[userId].pic;
+            subpost_info.user_text=usersData[userId].about;
+            subpost_info.user_name=usersData[userId].username;
+            subpost_info.user_post=postsData[update].comments[comment].content
+            subpost.push(subpost_info);
+        }
+        post.reply=subpost;
+    }
+    context.push(post);
+}
 var html    = template(context);
-$('#logged_user').append(html);
+$('#timeline').append(html);
 }
 
 function loadData(){
 
+
+var url= userJSONPath+"?callback=?";
+
 $.ajax({
-url: "https://raw.githubusercontent.com/MDiazC/socialnetwork-js-project/master/js/users.json",
-
-	type: "GET",
-	dataType: "jsonp",
-	success: function(respuesta){
-console.log("Recibes: ", respuesta);
-	}
+   type: 'GET',
+    url: url,
+    async: false,
+    jsonpCallback: 'jsonCallbackUser',
+    contentType: "application/json",
+    dataType: 'jsonp',
+    success: function(json) {
+        usersData=json;
+       loadUserInfo();
+    },
+    error: function(e) {
+       console.log(e.message);
+    }
 });
-$.getJSON( "https://raw.githubusercontent.com/MDiazC/socialnetwork-js-project/master/js/users.json", function( json ) {
-  console.log( "JSON Data: " + json );
- });
-var json = $.getJSON(userJSONPath);
-var usersData = eval("(" +json.responseText + ")");
-var json = $.getJSON(postsJSONPath);
-var postsData = eval("(" +json.responseText + ")");
 
+url= postsJSONPath+"?callback=?";
+
+$.ajax({
+   type: 'GET',
+    url: url,
+    async: false,
+    jsonpCallback: 'jsonCallbackPost',
+    contentType: "application/json",
+    dataType: 'jsonp',
+    success: function(json) {
+        postsData=json;
+        loadPosts();
+    },
+    error: function(e) {
+       console.log(e.message);
+    }
+});
 }
+
+
+
+
+
 
